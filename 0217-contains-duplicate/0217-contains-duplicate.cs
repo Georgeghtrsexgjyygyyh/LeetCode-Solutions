9b142667675d3c447fe3042c38997b68bb6571a4
George <95586665+Georgeghtrsexgjyygyyh@users.noreply.github.com>
2026-02-08 07:36:57 +0300

    Time: 22 ms (45.01%), Space: 73.9 MB (41.37%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cs
@@ -0,0 +1,16 @@
+public class Solution
+{
+    public bool ContainsDuplicate(int[] nums)
+    {
+        HashSet<int> visited = new HashSet<int>();
+
+        foreach (int x in nums) 
+        {
+            if(visited.Contains(x)) return true;
+
+            visited.Add(x);
+        }
+
+        return false;
+    }
+}
