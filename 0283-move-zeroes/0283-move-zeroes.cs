79d4debc0e5e1d4ee9de78aa37476b13d721d50c
George <95586665+Georgeghtrsexgjyygyyh@users.noreply.github.com>
2026-02-10 22:11:41 +0300

    Time: 1 ms (96.83%), Space: 58.4 MB (67.56%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.cs
@@ -0,0 +1,17 @@
+public class Solution {
+    public void MoveZeroes(int[] nums) {
+        int n = 0;
+
+        
+        for (int i = 0; i < nums.Length; i++) {
+            if (nums[i] != 0) {
+                nums[n++] = nums[i];
+            }
+        }
+
+        
+        for (int i = n; i < nums.Length; i++) {
+            nums[i] = 0;
+        }
+    }
+}
